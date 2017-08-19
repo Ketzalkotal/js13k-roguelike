38249ab90bf70184f34e662f73dfc339e72d76bd
James Daley <jdaley@dialsource.com>
2017-08-18 23:17:40 -0700

    initial game commit

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,24 @@
-import { HELLO } from './hello'
+var Ga = require('./ga')
+var Plugins = require('./ga-plugins')
 
-document.write(HELLO)
+var g = ga(512, 512, setup, []);
+
+var dungeon, player, treasure, enemies, chimes, exit,
+    healthBar, message, gameScene, gameOverScene;
+
+g.start();
+
+function setup(){
+	g.canvas.style.border = "1px blue dashed";
+	g.backgroundColor = "white";
+
+	g.state = play;
+}
+
+function play(){
+}
+
+function end() {
+  gameScene.visible = false;
+  gameOverScene.visible = true;
+}
